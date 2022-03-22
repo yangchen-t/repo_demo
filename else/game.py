aa6848c433ea143d2d6cf58e6c70f58597453053
xiangyang.chen <xiangyang.chen@westwell-lab.com>
2022-03-23 00:31:07 +0800

    trim

--- else/game.py
+++ else/game.py
@@ -6,17 +6,20 @@ class Game():
         def __init__(self,input):
             self.input = input
 
-        def game_bijiao(self):
+        def numbers_compute(self):
             sum = random.randint(0, 12)
             print(sum)
+
             if int(self.input) > int(sum) :
                 print('您的点数大于我，您赢！')
+
             elif int(self.input) < int(sum):
                 print('您的点数小于我，您输啦')
+
             else:
                 print('平局')
 
 
 game = Game(sys.argv[1])
-game.game_bijiao()
+game.numbers_compute()
 
