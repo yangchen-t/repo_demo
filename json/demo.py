37c94dc8fddb34c72244c63bd28bc14dd06b5a59
xiangyang.chen <xiangyang.chen@westwell-lab.com>
2022-03-25 18:27:42 +0800

    add json/demo.py

---
+++ json/demo.py
@@ -0,0 +1,31 @@
+x1_mission_202 = {
+    "header": {
+        "transId": "9263fcca-f139-4a5f-bc36-794b2ee2d70b",
+        "deviceId": "A021",
+        "timestamp": "20220320T190804682Z"
+    },
+    "body": {
+        "errorCode": "errorMsg: ",
+        "naviId": "A021-9c7fec46-13ed-44f9-bdbe-66ac67942cc3-0c094227-f6d9-491d-9a49-3fecc5b1fce1"
+    },
+    "statusCode": "202"
+}
+
+
+
+
+print(type(x1_mission_202))
+print(x1_mission_202['statusCode'])
+
+
+a = '{"id":"e325dc8f59320bb85c6d0324da56565e","url_token":"bu-xiang-lian-ai-49","name":"不想恋爱"}'
+d = eval(a)
+print(d)
+print(type(d))
+# print(d['id'])
+
+
+
+
+
+
