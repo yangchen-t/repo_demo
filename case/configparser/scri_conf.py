5ac2e56cd8976027060c92e2b843b41c095dca9b
xiangyang.chen <xiangyang.chen@westwell-lab.com>
2022-04-05 23:30:31 +0800

    update

--- case/configparser/scri_conf.py
+++
@@ -1,7 +0,0 @@
-#!/bin/python3
-
-import yaml
-
-f = open(r'/home/westwell/scripts/python/cxy/case/configparser/config.yaml')
-y = yaml.load(f)
-print(y)
--- log_on_test.py
+++
@@ -1,7 +0,0 @@
-
-
-data = {"username":"zhangfuhao","passwd":"zhang123456"}
-a = input(":")
-for i in  data.values():
-    if a == i:
-        print("ok")
