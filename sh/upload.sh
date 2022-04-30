c42662f6aab6f005940c915539f0b0cf2b0d39bb
xiangyang.chen <xiangyang.chen@westwell-lab.com>
2022-04-30 22:19:12 +0800

    modi repo

--- sh/upload.sh
+++ sh/upload.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 #read -p "Enter the vehicle number ~:" vehicle
+
 current_time="`date +%Y-%m-%d-%H-%M-%S`"
 time_now="`date +%Y-%m-%d' '%H:%M:%S`"
 start_time="`date -d "-3 hour" +%F' '%H:%M:%S`"
