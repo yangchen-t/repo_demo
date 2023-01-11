a299ce99ef4919804f71f4945a5a05611323e254
xiangyang.chen <xiangyang.chen@westwell-lab.com>
2023-01-11 16:12:16 +0800

    modify some code

--- Robot_Operating_System/ros2_test/View_linear_x.py
+++ Robot_Operating_System/ros2_test/View_linear_x.py
@@ -4,26 +4,20 @@ import os,time,datetime,pytz
 
 import rclpy
 from rclpy.node import Node
-from nav_msgs.msg import Odometry
+from sensor_msgs.msg import PointCloud2
 
 ns = os.getenv("QOMOLO_ROBOT_ID")
 
 class Subscriber(Node):
     def __init__(self,name):
         super().__init__(name)
-        self.subscriber = self.create_subscription(Odometry,"/"+ns+
-                                                  "/localization/odom",
+        self.subscriber = self.create_subscription(PointCloud2,
+                                                  "/pcl/output",
                                                   self.sub_callback,
                                                    10)
 
     def sub_callback(self,msg):
-        data  = (msg.twist.twist.linear.x)
-        with open("test.log",'a') as f:
-            f.write(datetime.datetime.now(pytz.timezone('PRC')).strftime("%Y-%m-%d_%H:%M:%S")+",")
-            f.write(str(time.time()) + ",")
-            f.write("current_linear_x = ")
-            f.write(str(data))
-            f.write(chr(10))
+        print(msg)
 def main(args=None):
     rclpy.init(args=args)
     node = Subscriber("test_sub_localization")
