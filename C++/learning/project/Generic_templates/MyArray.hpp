80fce3710fd7aa565837c3874afb97d318f552a1
xiangyang.chen <xiangyang.chen@westwell-lab.com>
2023-02-10 23:51:51 +0800

    modify Generic_templates

--- C++/learning/project/Generic_templates/MyArray.hpp
+++ C++/learning/project/Generic_templates/MyArray.hpp
@@ -12,28 +12,53 @@ public:
     // 有参构造
     MyArray(int capacity)
     {
+        // cout << "有参构造" << endl;
         this->C_Capacity = capacity;
         this->C_Size = 0;
-        this->pAddress = new T[this.C_Capacity];
+        this->pAddress = new T[this->C_Capacity];
     }
 
     // 拷贝构造
     MyArray(const MyArray & arr)
     {
+        // cout << "拷贝构造" << endl;
         this->C_Capacity = arr.C_Capacity;
         this->C_Size = arr .C_Size;
-        this->pAddress = new T[arr.C_Capacity]；  //  深拷贝
+        this->pAddress = new T[arr.C_Capacity];  //  深拷贝
         // 假设数组有数据情况下,进行数据拷贝
-        for (int i = 0; i < this,C_Size; i++)
+        for (int i = 0; i < this->C_Size; i++)
         {
-            this.pAddress[i] = arr->pAddress[i];
+            this->pAddress[i] = arr.pAddress[i];
         } 
+    }
+
+    // operator= 防止浅拷贝问题
+    MyArray & operator=(const MyArray & arr)
+    {
+        // cout << "operator=" << endl;
+        if (this->pAddress != NULL)
+        {
+            delete [] this->pAddress;
+            this->pAddress = NULL;
+            this->C_Capacity = 0;
+            this->C_Size = 0;
+        }
+        this->C_Capacity = arr.C_Capacity;
+        this->C_Size = arr .C_Size;
+        this->pAddress = new T[arr.C_Capacity];  
+        // 假设数组有数据情况下,进行数据拷贝
+        for (int i = 0; i < this->C_Size; i++)
+        {
+            this->pAddress[i] = arr.pAddress[i];
+        }
+        return * this;
     }
 
     ~MyArray()
     {
         if (this->pAddress != NULL)
         {
+            // cout << "析构" << endl;
             delete[] this->pAddress;
             this->pAddress =  NULL;
         }
--- C++/learning/project/Generic_templates/main.cpp
+++ C++/learning/project/Generic_templates/main.cpp
@@ -1,11 +1,22 @@
 #include <iostream>
-
+#include "MyArray.hpp"
 
 using namespace std;
 
+void test01()      // 因为是函数数据 在栈上，执行完毕会自动释放
+{
+    MyArray<int> arr1(5);     //  有参构造
+    MyArray<int> arr2(arr1);  //  拷贝构造
+    MyArray<int> arr3(100);   //  有参构造
+    arr3 = arr1;              //  opertor=
+}
+void test02()
+{
+}
 
 int main()
 {
-    cout << "test" << endl;
+    test01();
+    test02();
     return 0;
 }
