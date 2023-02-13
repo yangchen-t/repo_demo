81114b07b0c3d0abb7200cd6547e149095d6eb97
xiangyang.chen <xiangyang.chen@westwell-lab.com>
2023-02-13 22:58:21 +0800

    add golang

--- Python/pyautogui/current_time_size.py
+++ Python/pyautogui/current_time_size.py
@@ -1,9 +1,11 @@
 #/bin/python3
 import pyautogui
+import time 
 
 try:
     while True:
         x,y = pyautogui.position()
+        time.sleep(1)
         print(x, y)
 except KeyboardInterrupt:
     print('\nExit.')
