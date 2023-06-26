3193b7952814ffa221219e3c2fd987e827351354
xiangyang.chen <xiangyang.chen@westwell-lab.com>
2023-06-26 10:32:24 +0800

    update cmake

--- cmake_test/main.cpp
+++ cmake_test/main.cpp
@@ -1,16 +1,22 @@
-#include <iostream>
-// #include "build/config.h"
-#include "include/getTime.h"
-#include "add/add.h"
 
-// #ifdef USE_MYADD
-//     #include "add/add.h"
-// #else
-//     #include "include/getTime.h"
-// #endif
+#include "include/getTime.h"
+#include <functional>
+
+#include "build/config.h"
+#include "add/add.h"
 
 int main()
 {
-        std::cout << "test print:" << Add(1,2)<< std::endl;
+
+    if (flag)
+    {
+        std::cout << "YES is true" << std::endl;
+        std::cout << "test print:" << Add(1, 20) << std::endl;
+    }
+    else
+    {
+        std::plus<int> add;
+        std::cout << add(1, 2) << std::endl;
         gettime();
+    }
 }
